e6779d555b2cd51d3401f00936d4ddf70f0351c2
calebomusic <calebomusic@gmail.com>
2017-06-10 20:16:07 -0700

    Complete lengthy const list of focus areas and focus area mapping object

--- index.js
+++ index.js
@@ -2,8 +2,44 @@ import * as d3 from 'd3';
 
 d3.csv('databasexls.csv', function(data) {
   data.forEach((datum, index) => {
-    let paragraph = document.createElement('p')
+    let paragraph = document.createElement('p');
     paragraph.textContent = `${index + 1}) ${datum.Grant} | ${datum["Grant Amount"]}`;
     document.body.appendChild(paragraph);
+    console.log(datum["Grant Amount"])
+    datum["Grant Amount"] = parseInt(datum["Grant Amount"].replace(/\D/g, ""));
+    console.log(datum["Grant Amount"])
+    FOCUS_AREA_MAPPING[datum["Focus Area"]].push(datum);
   });
 });
+
+const GLOBAL_HEALTH = [];
+const BIOSECURITY = [];
+const CRIMINAL_JUSTICE = [];
+const FARM_ANIMAL_WELFARE = [];
+const HISTORY_OF_PHIL = [];
+const LAND_USE = [];
+const MACROECONOMIC_POLICY = [];
+const OTHER_AREAS = [];
+const RISKS_FROM_AI = [];
+const INTELLIGENCE = [];
+const SCIENTIFIC_RESEARCH = [];
+const CATASTROPHIC_RISKS = [];
+const IMMIGRATION = [];
+const USPOLICY = [];
+
+// Maps focus area string to relevant const
+const FOCUS_AREA_MAPPING = {
+  "Criminal Justice Reform": CRIMINAL_JUSTICE,
+  "Farm Animal Welfare": FARM_ANIMAL_WELFARE,
+  "Macroeconomic Stabilization Policy": MACROECONOMIC_POLICY,
+  "Immigration Policy": IMMIGRATION,
+  "Land Use Reform": LAND_USE,
+  "History of Philanthropy": HISTORY_OF_PHIL,
+  "Biosecurity and Pandemic Preparedness": BIOSECURITY,
+  "Potential Risks from Advanced Artificial Intelligence": RISKS_FROM_AI,
+  "Global Catastrophic Risks": CATASTROPHIC_RISKS,
+  "Scientific Research": SCIENTIFIC_RESEARCH,
+  "Global Health & Development": GLOBAL_HEALTH,
+  "Other areas": OTHER_AREAS,
+  "U.S. Policy": USPOLICY
+};
