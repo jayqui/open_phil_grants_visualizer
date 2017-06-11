4814780c03e85ec7e1f4976704be547d3cbf7f37
calebomusic <calebomusic@gmail.com>
2017-06-11 11:15:49 -0700

    Increase pie size, fix colors, add ugly labels

--- drawPie.js
+++ drawPie.js
@@ -5,8 +5,8 @@ import * as d3 from 'd3';
 export default function (name, grantObjArr) {
   appendDiv(name, function() {
     var svg = d3.select("#" + name).append("svg:svg"),
-        width = 500,
-        height = 500,
+        width = 700,
+        height = 700,
         radius = Math.min(width, height) / 2,
         g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
@@ -34,17 +34,18 @@ export default function (name, grantObjArr) {
 
     arc.append("path")
       .attr("d", path)
-      .attr("fill", d => color(d))
+      .attr("fill", (d, i) => color(i))
 
     arc.append("text")
+      .data(pie(grantObjArr))
       .attr("transform", (d) => "translate(" + label.centroid(d) + ")")
-      .text(d => d["Organization Name"])
+      .text(d => d.data["Organization Name"] + ", $" + d.data["Grant Amount"])
   });
 }
 
 function randomColor() {
   const ColorStr = ["#"];
-  const CHARS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g"];
+  const CHARS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
 
   for(let i = 0; i < 6; i++) {
     let randIdx = Math.floor(Math.random() * CHARS.length);
