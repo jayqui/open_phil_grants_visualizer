7a4b416bc512f70019e65a800f922e8c9ba0feba
calebomusic <calebomusic@gmail.com>
2017-06-24 17:35:54 -0700

    Begin center text

--- drawPie.js
+++ drawPie.js
@@ -2,42 +2,72 @@ import * as d3 from 'd3';
 
 export default function (id, grantGroup) {
   appendDiv(id, function () {
-    var svg = d3.select("#" + id).append("svg:svg"),
-        width = 700,
-        height = 700,
+    let selectedSliceText;
+    let sum = grantGroup.reduce((s, g) => s + g['Grant Amount'], 0),
+        svg = d3.select("#" + id).append("svg:svg"),
+        margin = {top: 50, right: 50, bottom: 50, left: 50},
+        width = 500 + Math.pow(Math.log(sum , 2), 2) - margin.left,
+        height = 500 + Math.pow(Math.log(sum, 2), 2) - margin.top,
         radius = Math.min(width, height) / 2,
-        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        g = svg.append("g").attr("transform", "translate(" +
+                                  width / 2 + margin.left + "," +
+                                  height / 2 +  margin.top + ")"
+                                );
 
     svg.attr("width", width).attr("height", height);
 
-    var pie = d3.pie()
+    let pie = d3.pie()
           .sort(null)
           .value((d) => d["Grant Amount"])
 
-    var color = d3.scaleOrdinal(nRandomColors(grantGroup.length));
+    let color = d3.scaleOrdinal(nRandomColors(grantGroup.length));
 
-    var path = d3.arc()
-          .outerRadius(radius - 10)
-          .innerRadius(0)
+    let path = d3.arc()
+          .outerRadius(radius - 60)
+          .innerRadius(100)
+          .cornerRadius(10)
 
-    var label = d3.arc()
+    let label = d3.arc()
           .outerRadius(radius - 40)
           .innerRadius(radius - 40)
 
-    var arc = g.selectAll(".arc")
+    let arc = g.selectAll(".arc")
           .data(pie(grantGroup))
           .enter()
             .append("g")
             .attr("class", "arc-" + id)
 
+    // New plan: fit grant description, title and links in the center of the pie chart
+    arc.append("text")
+      .data(pie(grantGroup))
+      .attr("transform", (d) => {
+        let center = path.centroid(d),
+            x = center[0],
+            y = center[1],
+            h = Math.sqrt(x * x + y * y);
+
+        return "translate(" + Math.floor((x / h)) + 30 + "," +
+                              Math.floor((y / h)) + ")";
+      })
+      .attr("id", (d) => id + "-text-" + d.data["Organization Name"].toLocaleString().replace(/\W|\s/g, ""))
+      .style("display", "none")
+      .html(d => d.data["Organization Name"].toLocaleString())
+
     arc.append("path")
       .attr("d", path)
       .attr("fill", (d, i) => color(i))
+      .on("mouseover", (d, i) => centerText(d, i))
 
-    arc.append("text")
-      .data(pie(grantGroup))
-      .attr("transform", (d) => "translate(" + label.centroid(d) + ")")
-      .text(d => d.data["Organization Name"] + ", $" + d.data["Grant Amount"].toLocaleString())
+    function centerText(d,i) {
+      let text = d3.select("#" + id + "-text-" + d.data["Organization Name"].toLocaleString().replace(/\W|\s/g, ""));
+
+      if(!selectedSliceText || text.attr("id") !== selectedSliceText.attr("id")) {
+        if(selectedSliceText) selectedSliceText.style("display", "none");
+        text.style("display", "block");
+        selectedSliceText = text;
+      }
+    }
+
   });
 }
 
