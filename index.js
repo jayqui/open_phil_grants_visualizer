a1bb75b268ff49950eef5908ec47e9f16143e18d
calebomusic <calebomusic@gmail.com>
2017-07-02 18:28:26 -0700

    Add anchor tags for sidenav

--- index.js
+++ index.js
@@ -11,18 +11,20 @@ d3.csv('databasexls.csv', function(data) {
 
 function listDataByFocusArea(dataByFocusArea) {
   Object.keys(dataByFocusArea).forEach((focusArea, index) => {
-    appendNewElement(textElement('h1', focusArea));
-
-    let grantGroup = dataByFocusArea[focusArea];
-    // listDataInGrantGroup(grantGroup);
+    let id = focusArea.replace(/\W|\s/g, "").toLowerCase(),
+        grantGroup = dataByFocusArea[focusArea];
 
     grantGroup = deleteUndisclosed(grantGroup);
+
+    appendNewTextElement(textElement('h1', focusArea));
+    appendNewAnchorElement(id);
+
+    // listDataInGrantGroup(grantGroup);
 
     grantGroup.forEach(grant =>
       grant["Grant Amount"] = parseInt(grant["Grant Amount"].replace(/\D/g, ""))
     );
 
-    let id = focusArea.replace(/\W|\s/g, "");
     drawPie(id, grantGroup);
   })
 }
@@ -41,9 +43,15 @@ function deleteUndisclosed(grantGroup) {
 
 // function listDataInGrantGroup(grantGroup) {
 //   grantGroup.forEach((grantData, index) => {
-//     appendNewElement(textElement('p', grantText(grantData, index)));
+//     appendNewTextElement(textElement('p', grantText(grantData, index)));
 //   });
 // }
+
+function appendNewAnchorElement(href) {
+  let a = document.createElement("a");
+  a.href = href;
+  document.body.appendChild(a);
+}
 
 function textElement(htmlTag, text) {
   let newElement = document.createElement(htmlTag)
@@ -51,7 +59,7 @@ function textElement(htmlTag, text) {
   return newElement;
 }
 
-function appendNewElement(textElement) {
+function appendNewTextElement(textElement) {
   document.body.appendChild(textElement);
 }
 
