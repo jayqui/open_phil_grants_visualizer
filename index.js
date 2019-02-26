b6396d93b13b26d761f2170db0895ba67a025221
Jay Quigley <quigley.james.g@gmail.com>
2019-02-26 15:15:24 -0800

    yarn add d3-fetch

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const cron = require('node-cron');
 const express = require('express');
 const fs = require('fs');
 const moment = require('moment');
+const fetch = require('fetch');
+const d3 = require('d3-fetch');
+if (typeof fetch !== 'function') {
+  global.fetch = require('node-fetch-polyfill');
+}
 
 app = express();
+
+d3.csv('https://www.openphilanthropy.org/giving/grants/spreadsheet').then(data => {
+  console.log(data);
+})
 
 // schedule tasks to be run on the server
 cron.schedule("* * * * *", function() {
