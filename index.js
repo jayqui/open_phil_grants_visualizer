b15fca32db855fb79fb1c668bb7698b8ae7e9b86
Jay Quigley <quigley.james.g@gmail.com>
2019-02-26 15:04:23 -0800

    yarn add moment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const cron = require('node-cron');
 const express = require('express');
 const fs = require('fs');
+const moment = require('moment');
 
 app = express();
 
 // schedule tasks to be run on the server
 cron.schedule("* * * * *", function() {
-  console.log("running a task every minute");
+  console.log(`The time is ${moment().hour()}:${moment().minute()}:${moment().second()}`);
 });
 
 app.listen(3128);
