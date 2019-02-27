daf1148ab5c83ca8ce97aca29f6719e3c4e5850d
Jay Quigley <quigley.james.g@gmail.com>
2019-02-26 17:03:20 -0800

    we're now displaying data in the browser

--- open_phil_grants_visualizer/src/App.js
+++ open_phil_grants_visualizer/src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3-fetch';
 
 class App extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    // FOR PRODUCTION
+    // d3.csv('https://www.openphilanthropy.org/giving/grants/spreadsheet').then(data => {
+
+    // TEMP FOR DEVELOPMENT
+    d3.csv('/giving/grants/spreadsheet').then(data => {
+      this.setState({ data });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <div>
+            {this.state.data.length && this.state.data.map(datum => (
+              <div key={`${datum["Grant"]}${datum["Date"]}`}>
+                <h3>{datum["Grant"]}</h3>
+                <p>{datum["Organization Name"]}</p>
+                <p>{datum["Focus Area"]}</p>
+                <p>{datum["Amount"]}</p>
+                <p>{datum["Date"]}</p>
+              </div>
+            ))}
+          </div>
         </header>
       </div>
     );
